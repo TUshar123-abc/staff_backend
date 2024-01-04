9fb0cb11ee2ed0b9e7206ec0aaad5bb264627f64
TusharMore <tusharmore2ab@gmail.com>
2024-01-04 13:53:24 +0530

    final commit

---
+++ app.js
@@ -0,0 +1,17 @@
+import express  from "express";
+import dotenv from "dotenv"
+import db from "./config/db.js"
+import  router  from "./routes/contactRoutes.js";
+import cors from "cors"
+
+const app = express();
+const port = process.env.PORT || 4000;
+dotenv.config();
+db()
+app.use(cors())
+app.use(express.json());
+app.use('/api', router)
+
+app.listen(port, "0.0.0.0" , ()=>{
+    console.log(`Server is running on http://localhost:${port}`)
+})
