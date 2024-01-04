eacda9208f53d67adb8526bc14a94d36b04c079a
TusharMore <tusharmore2ab@gmail.com>
2024-01-04 15:05:13 +0530

    port changed

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ import  router  from "./routes/contactRoutes.js";
 import cors from "cors"
 
 const app = express();
-const port = process.env.PORT || 2500;
+const port = process.env.PORT || 8080;
 dotenv.config();
 db()
 app.use(cors())
 app.use(express.json());
 app.use('/api', router)
 
-app.listen(port, "0.0.0.0" , ()=>{
+app.listen(port, ()=>{
     console.log(`Server is running on http://localhost:${port}`)
 })
